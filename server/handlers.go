da6b54e7173513ea3a39b5f00941f425864a056a
Dexter Haslem <dmh@fastmail.com>
2018-01-04 22:39:48 -0700

    redirect on upload. need to wait for it to finish eventually

--- server/handlers.go
+++ server/handlers.go
@@ -12,7 +12,7 @@ import (
 	"io"
 	"os"
 	"time"
-	_ "html"
+	"html"
 )
 
 // TEMP: always parse template, makes editing easier during development
@@ -147,47 +147,66 @@ func (s *Server) replyHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) addReplyHandler(w http.ResponseWriter, r *http.Request) {
-	// todo: lookup board max
-	r.Body = http.MaxBytesReader(w, r.Body, 1024*1024*8)
-
-	sentFile, fileHeader, err := r.FormFile("f")
-	if err != nil {
-		http.Error(w, "Invalid method", http.StatusBadRequest)
-		return
-	}
-
 	v := mux.Vars(r)
 	bn := v["board"]
-	saveDir := filepath.Join(s.Config.StaticDir, bn)
-	if err := os.MkdirAll(saveDir, 0700); err != nil {
-		return
+	tidstr := v["thread"]
+	tid, _ := strconv.ParseInt(tidstr, 10, 64)
+	newPost := &model.Post{}
+
+	// todo: lookup board max
+	r.Body = http.MaxBytesReader(w, r.Body, 1024*1024*8)
+	sentFile, fileHeader, err := r.FormFile("f")
+	if err == nil {
+		// todo: abstract
+		saveDir := filepath.Join(s.Config.StaticDir, bn)
+		if err := os.MkdirAll(saveDir, 0700); err != nil {
+			return
+		}
+
+		// ensure dir for board content exists
+		// security: we could get in a bogus header with traversal exploitation so chop to base
+		// dont use original filename, timestamp fn := filepath.Base(fileHeader.Filename)
+		origExt := filepath.Ext(fileHeader.Filename)
+		fn := fmt.Sprintf("%d%s", time.Now().Unix(), origExt)
+		saveFn := filepath.Join(saveDir, fn)
+		saveFile, err := os.Create(saveFn)
+		if err != nil {
+			return
+		}
+
+		newPost.OriginalFilename = filepath.Base(fileHeader.Filename)
+		newPost.Size = fileHeader.Size
+		// !! CDN HOOK
+		newPost.Location = filepath.Join("/", saveFn)
+		// TODO: thumbnail creation
+		newPost.ThumbnailLocation = newPost.Location
+
+		// start saving it to disk. this is dumb atm
+		go func() {
+			defer r.Body.Close()
+			defer sentFile.Close()
+			defer saveFile.Close()
+			// TODO: partial writes
+			io.Copy(saveFile, sentFile)
+		}()
 	}
 
-	// ensure dir for board content exists
-	// security: we could get in a bogus header with traversal exploitation so chop to base
-	// dont use original filename, timestamp fn := filepath.Base(fileHeader.Filename)
-	origExt := filepath.Ext(fileHeader.Filename)
-	fn := fmt.Sprintf("%d%s", time.Now().Unix(), origExt)
-	saveFn := filepath.Join(saveDir, fn)
-	saveFile, err := os.Create(saveFn)
+	postContent := r.FormValue("post")
+	newPost.Content = html.EscapeString(postContent)
+	newPost.ThreadID = tid
+	newPost.PostedAt = time.Now()
+
+	// TODO: inet only handles ipv4. argh, inet6 localhost coming in
+	// not sure if i should even bother with ip based user ids. might have to text
+	_, err = s.Db.CreatePost(newPost, "192.168.1.1") //r.RemoteAddr)
 	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	// start saving it to disk. this is dumb atm
-	go func() {
-		defer r.Body.Close()
-		defer sentFile.Close()
-		defer saveFile.Close()
-		// TODO: partial writes
-		io.Copy(saveFile, sentFile)
-	}()
-
-	//postContent := r.FormValue("post")
-
-	//newPost := &model.Post{}
-	//newPost.Content = html.EscapeString(postContent)
-	//newPost.
+	// TODO: hardcoded proto atm :-(
+	url := fmt.Sprintf("http://%s/%s/%d", r.Host, bn, tid)
+	http.Redirect(w, r, url, http.StatusSeeOther)
 	//b, thread := s.tryGetBoardAndThread(w, r)
 	//if b == nil || thread == nil {
 	//	return
