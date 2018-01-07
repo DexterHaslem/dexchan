67e26feceb23add7e55dd1bae2aa01a1e829d0b3
Dexter Haslem <dmh@fastmail.com>
2018-01-07 01:01:33 -0700

    actually, keep track of id of whatever they were doing. cant constrain, maybe add check later
    editing migration directly since not yet pushed

--- db/sql/V4__AddActionLog.sql
+++ db/sql/V4__AddActionLog.sql
@@ -1,17 +1,19 @@
 CREATE TABLE IF NOT EXISTS log_action (
-  id       SERIAL PRIMARY KEY,
-  auser_id INT REFERENCES auser (id) NOT NULL,
-  action   TEXT,
-  ts       TIMESTAMP
+  id        SERIAL PRIMARY KEY,
+  auser_id  INT REFERENCES auser (id) NOT NULL,
+  action    TEXT,
+  entity_id INT                       NOT NULL,
+  ts        TIMESTAMP
 );
 
-CREATE OR REPLACE FUNCTION add_action_log(ip TEXT, action TEXT)
+CREATE OR REPLACE FUNCTION add_action_log(ip TEXT, action TEXT, entity_id INT)
   RETURNS VOID AS
 $func$
-INSERT INTO log_action (auser_id, action, ts)
+INSERT INTO log_action (auser_id, action, entity_id, ts)
   SELECT
     (SELECT get_auser(ip)),
     $2,
+    $3,
     now();
 $func$
 LANGUAGE SQL;
--- server/db/db.go
+++ server/db/db.go
@@ -18,7 +18,7 @@ type D interface {
 	GetThreads(boardID int64) ([]*model.Thread, error)
 	GetThread(threadID int64) (*model.Thread, error)
 	GetPosts(threadID int64) ([]*model.Post, error)
-	AddActionLog(ip string, action string) error
+	AddActionLog(ip string, action string, entityID int64) error
 	Close() error
 }
 
@@ -32,8 +32,8 @@ func (d *db) GetUserID(ip string) (int64, error) {
 	return userID, err
 }
 
-func (d *db) AddActionLog(ip string, action string) error {
-	return d.openedDB.QueryRow(`SELECT add_action_log($1, $2)`, ip, action).Scan()
+func (d *db) AddActionLog(ip string, action string, entityID int64) error {
+	return d.openedDB.QueryRow(`SELECT add_action_log($1, $2, $3)`, ip, action, entityID).Scan()
 }
 
 func (d *db) GetBoards() ([]*model.Board, error) {
@@ -85,7 +85,7 @@ func (d *db) CreatePost(p *model.Post, ip string) (int64, error) {
 		p.ThreadID, p.Content, time.Now(), postedByID, false,
 		p.OriginalFilename, p.ThumbnailLocation, p.Location, p.Size).Scan(&createdID)
 
-	d.AddActionLog(ip, "created_post")
+	d.AddActionLog(ip, "created_post", createdID)
 	return createdID, err
 }
 
@@ -103,7 +103,7 @@ func (d *db) CreateThread(t *model.Thread, ip string) (int64, error) {
 		t.BoardID, t.Subject, t.Description, time.Now(), postedByID, false,
 		t.OriginalFilename, t.ThumbnailLocation, t.Location, t.Size).Scan(&createdID)
 
-	d.AddActionLog(ip, "created_thread")
+	d.AddActionLog(ip, "created_thread", createdID)
 	return createdID, err
 }
 
